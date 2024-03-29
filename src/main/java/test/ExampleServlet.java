c2da7828339d208381a418bf5074325e5aeb8597
zhangzhenyu201403 <161813505+zhangzhenyu201403@users.noreply.github.com>
2024-03-30 06:17:54 +0800

    测试22

--- src/main/java/test/ExampleServlet.java
+++ src/main/java/test/ExampleServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.IOException;
 
 public class ExampleServlet extends HttpServlet
@@ -11,8 +12,10 @@ public class ExampleServlet extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
     {
-        res.getWriter().write("Hello world11112222");
+        res.getWriter().write("Hello world11112222111");
         res.getWriter().close();
 
+
+        System.out.println("Hello");
     }
 }
---
+++ src/main/java/test/test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * @author: create by zhangzhenyu
+ * @version: v1.0
+ * @description: test
+ * @date:2024/3/30 6:15 AM
+ **/
+public class test {
+}
