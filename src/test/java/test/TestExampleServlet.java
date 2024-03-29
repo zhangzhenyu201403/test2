bf95fefd721a6688e5c0a88c504f27d6ccfc64d9
zhangzhenyu <zhangzhenyu@longfor.com>
2024-03-29 15:08:37 +0800

    测试

---
+++ src/test/java/test/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package test;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link test.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
